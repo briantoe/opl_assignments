d8dd6e2c8791998922d74b5644aa0a281eba4724
BrianT <bht170030@utdallas.edu>
2019-10-06 21:57:12 -0500

    finished problem 6

--- assignment2/assignment2.rkt
+++ assignment2/assignment2.rkt
@@ -1,11 +1,32 @@
 #lang racket
 
+; problem 4
 (define (last-element ls)
   (cond ((null? (cdr ls))
          (car ls))
   (else (last-element (cdr ls)))))
 
+; problem 5
+(define (deep-reverse ls)
+  (if (list? ls)
+      (reverse (map deep-reverse ls))
+      ls))
+; problem 6
+(define (sum-odd-squares ls)
+  (cond 
+        ((null? ls) 0)
+        ((odd? (car ls)) (+ (* (car ls) (car ls)) (sum-odd-squares (cdr ls))))
+        ((even? (car ls)) (+ 0 (sum-odd-squares (cdr ls))))
+        
+        ))
 
+; problem 7
+;(define (subsets ls)
+;  (if (null? s)
+;     (list nil)
+;     (
+
+; problem 10
 (define (fib n)
   (if (= n 1)
     0
